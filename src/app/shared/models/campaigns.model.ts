f13524fc6f2d26d2dd10fed665b9d6d4ed6f6a3a
kojdj0811 <kojdj0811@gmail.com>
2022-06-07 01:14:08 +0900

    챕터명 수정

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -8,7 +8,7 @@ export const Campaigns = [
     logs: [
       {
         index: '8', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
-        name: 'Chapter 08', // 저장소에서 표시할 파일 이름
+        name: '- Chapter 08', // 저장소에서 표시할 파일 이름
         images: [
           {
             id: `-N3ssnljJN0Fe3oEosr7`, // 롤20 로그 html에서 data-messageid 부분이다. 그 아래로 내려갈 시 아래에 적힌 이름의 이미지로 바뀐다.
