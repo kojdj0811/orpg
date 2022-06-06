142a2c8445a061806f540a9dc87fff2889923839
kojdj0811 <kojdj0811@gmail.com>
2022-06-06 22:41:50 +0900

    test3

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -8,7 +8,7 @@ export const Campaigns = [
     logs: [
       {
         index: '1', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
-        name: '2022-06-01', // 저장소에서 표시할 파일 이름
+        name: '1화', // 저장소에서 표시할 파일 이름
         images: [
         ],
         interfaces: [
