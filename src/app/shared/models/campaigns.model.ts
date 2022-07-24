777385db6590511240e34015d40b9727275729f1
kojdj0811 <kojdj0811@gmail.com>
2022-07-24 23:55:47 +0900

    14 추가

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -410,6 +410,14 @@ export const Campaigns = [
         interfaces: [
         ],
       },
+      {
+        index: '14', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
+        name: '- Chapter 14', // 저장소에서 표시할 파일 이름
+        images: [
+        ],
+        interfaces: [
+        ],
+      },
     ]
   },
   {
