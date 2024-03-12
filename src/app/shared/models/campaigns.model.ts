be48369995b74ccde801d307bd1e8f58977cc545
kojdj0811 <kojdj0811@gmail.com>
2024-03-13 00:09:49 +0900

    update chapter 3

--- src/app/shared/models/campaigns.model.ts
+++ src/app/shared/models/campaigns.model.ts
@@ -682,7 +682,14 @@ export const Campaigns = [
         interfaces: [
         ],
       },
-    ]
+      {
+        index: '3', // orpg\src\campaigns\TEST\logs 폴더 내에 있는 html 파일 이름
+        name: '- Chapter 03', // 저장소에서 표시할 파일 이름
+        images: [
+        ],
+        interfaces: [
+        ],
+      },    ]
   },
 
   {
